1e7e039b81b1f08414d87033f765beec10173d59
ꋬꋪꋖꌈꋪꁲꌚ ꂟꋪꌈꂵꌚꋖꁲ <drumsta@users.noreply.github.com>
2023-05-02 23:30:05 +0000

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -37,8 +37,25 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     styled: true,
-    themes: false,
-    base: false,
+    themes: [
+      {
+        light: {
+          // @ts-ignore
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#004fc2",
+          "base-100": "#fcfcfc",
+        },
+      },
+      {
+        dark: {
+          // @ts-ignore
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          primary: "#004fc2",
+          "base-100": "#1a1a1a",
+        },
+      },
+    ],
+    base: true,
     utils: true,
     logs: true,
     rtl: false,
