9acea8884a07680abb4f51900f777a80500e04cb
ꋬꋪꋖꌈꋪꁲꌚ ꂟꋪꌈꂵꌚꋖꁲ <drumsta@users.noreply.github.com>
2023-05-03 19:21:04 +0000

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,29 +9,6 @@ module.exports = {
       fontSize: {
         xxs: ".5rem",
       },
-      colors: {
-        "color-text": "var(--color)",
-        "color-text-subtle": "var(--color-subtle)",
-        "color-text-accent": "var(--color-accent)",
-        "color-bg": "var(--color-bg)",
-        "color-bg-subtle": "var(--color-bg-subtle)",
-        "color-bg-accent": "var(--color-bg-accent)",
-        "color-border": "var(--color-border)",
-        "color-border-subtle": "var(--color-border-subtle)",
-        "color-border-accent": "var(--color-accent)",
-        "color-light": "var(--color-light)",
-        "color-dark": "var(--color-dark)",
-        "color-primary-dark": "var(--color-primary-dark)",
-        "color-primary-light": "var(--color-primary-light)",
-        "color-secondary-dark": "var(--color-secondary-dark)",
-        "color-secondary-light": "var(--color-secondary-light)",
-        "color-positive-dark": "var(--color-positive-dark)",
-        "color-positive-light": "var(--color-positive-light)",
-        "color-negative-dark": "var(--color-negative-dark)",
-        "color-negative-light": "var(--color-negative-light)",
-        "color-warning-dark": "var(--color-warning-dark)",
-        "color-warning-light": "var(--color-warning-light)",
-      },
     },
   },
   plugins: [require("daisyui")],
@@ -40,7 +17,7 @@ module.exports = {
     themes: [
       {
         light: {
-          // @ts-ignore
+          // @ts-expect-error
           ...require("daisyui/src/colors/themes")["[data-theme=light]"],
           primary: "#004fc2",
           "base-100": "#fcfcfc",
@@ -48,7 +25,7 @@ module.exports = {
       },
       {
         dark: {
-          // @ts-ignore
+          // @ts-expect-error
           ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
           primary: "#004fc2",
           "base-100": "#1a1a1a",
