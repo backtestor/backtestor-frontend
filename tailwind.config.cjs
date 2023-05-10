ae615374c7df728e0fa2abebade984e28eaef85c
ꋬꋪꋖꌈꋪꁲꌚ ꂟꋪꌈꂵꌚꋖꁲ <drumsta@users.noreply.github.com>
2023-05-10 21:25:47 +0100

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ts,astro}"],
+  content: ["./public/**/*.html", "./src/**/*.{ts,astro}"],
   theme: {
     extend: {
       cursor: {
@@ -9,34 +9,31 @@ module.exports = {
       fontSize: {
         xxs: ".5rem",
       },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        base: "hsl(var(--base) / <alpha-value>)",
+        "base-subtle": "hsl(var(--base-subtle) / <alpha-value>)",
+        content: "hsl(var(--content) / <alpha-value>)",
+        "content-subtle": "hsl(var(--content-subtle) / <alpha-value>)",
+        neutral: "hsl(var(--neutral) / <alpha-value>)",
+        "neutral-focus": "hsl(var(--neutral-focus) / <alpha-value>)",
+        "neutral-content": "hsl(var(--neutral-content) / <alpha-value>)",
+        "color-content": "hsl(var(--color-content) / <alpha-value>)",
+        "color-primary": "hsl(var(--color-primary) / <alpha-value>)",
+        "color-primary-focus": "hsl(var(--color-primary-focus) / <alpha-value>)",
+        "color-secondary": "hsl(var(--color-secondary) / <alpha-value>)",
+        "color-secondary-focus": "hsl(var(--color-secondary-focus) / <alpha-value>)",
+        "color-positive": "hsl(var(--color-positive) / <alpha-value>)",
+        "color-positive-focus": "hsl(var(--color-positive-focus) / <alpha-value>)",
+        "color-negative": "hsl(var(--color-negative) / <alpha-value>)",
+        "color-negative-focus": "hsl(var(--color-negative-focus) / <alpha-value>)",
+        "color-warning": "hsl(var(--color-warning) / <alpha-value>)",
+        "color-warning-focus": "hsl(var(--color-warning-focus) / <alpha-value>)",
+      },
     },
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    styled: true,
-    themes: [
-      {
-        light: {
-          // @ts-expect-error
-          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
-          primary: "#004fc2",
-          "base-100": "#fcfcfc",
-        },
-      },
-      {
-        dark: {
-          // @ts-expect-error
-          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
-          primary: "#004fc2",
-          "base-100": "#1a1a1a",
-        },
-      },
-    ],
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
+  corePlugins: {
+    preflight: false,
   },
 };
